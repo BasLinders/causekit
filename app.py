5bc57ea61268cb7c2ade95a0bb3b018e33453e09
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2026-06-26 16:53:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Causekit", layout="wide")
 
 st.title("Causekit")
-st.write("### <span style='color: orange;'>v1.0</span>", unsafe_allow_html=True)
 st.caption("Causal inference toolkit for CRO teams.")
+st.write("### <span style='color: orange;'>v1.0</span>", unsafe_allow_html=True)
 
 st.markdown(
     """
